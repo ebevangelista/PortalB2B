05d40914b13bd5bf1afeec8886e14774c90db28c
Raul Kaio <rkaio18@gmail.com>
2017-09-20 19:47:36 -0300

    Commit inicial
    Commit inicial.

---
+++ login.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+
+	<!-- Google fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+	
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+	<!--  MENU SOCKET TOPO DE LIMITE DE CRÉDITO E LOGIN  -->
+	<div class="container-fluid">
+		
+	
+	<!--  CONTAINER PRINCIPAL  -->
+	<div class="row">
+			<div class="col-md-4">.</div>
+			<div class="col-md-4 text-center">
+				<div id="divLogin" class="wrapper">
+					<form class="form-signin">       
+					  <img src="img/logotipo.jpg" alt="" class="img-rounded" width="270" height="97">
+					  <br>
+					  <br>
+					  <br>
+					  <input type="text" class="form-control text-center" name="email" placeholder="E-mail" required="" /><br>
+					  <input type="password" class="form-control text-center" name="senha" placeholder="Senha" required="" />      
+					  <label class="checkbox">
+						<input type="checkbox" value="remember-me" id="rememberMe" name="rememberMe">Lembrar-me
+					  </label>
+					  <button class="btn btn-lg btn-primary btn-block" type="submit">ENTRAR</button>   
+					</form>
+					<br>
+					<a class="text-center">Esqueci a senha</a>
+					<br>
+					<a class="text-center">Já é cliente? Solicite acesso</a>
+				</div>
+			</div>
+			<div class="col-md-4">.</div>
+	</div>
+	
+	<!-- RODAPÉ -->
+	<div id="divFooter" class="row">
+		<div class="col-md-1">	
+		</div>
+		<div class="col-md-3">
+			<h3>ONDE ESTAMOS</h3>
+			<span>
+			<strong class="text-center">Mato Grosso do Sul</strong><br>
+			Av. João Batista Fernandes, 1415<br>
+			Polo Empresarial Oeste<br>
+			Campo Grande - MS<br>
+			Fone: (67) 3378-2000<br>
+			Fax: (67) 3378-2001<br>
+			</span>
+		</div>
+		<div class="col-md-3">
+			<h3>&nbsp;</h3>
+			<span>
+			<strong class="text-center">Mato Grosso</strong><br>
+			R. Ipê Amarelo, QD 06 - LT 0<br>
+			Bairro Novo Mundo<br>
+			Várzea Grande - MT<br>
+			Fone: (65) 3688-3900<br>
+			Fax: (65) 3688-3900<br>
+			</span>
+		</div>
+		<div class="col-md-4">
+			<h3>FALE CONOSCO</h3>
+			<span>
+			De segunda à sexta<br>
+			Das 08h às 18h<br>
+			<h1>(11) 4935-2344</h1>
+			compras@distlopes.com.br<br>
+			</span>
+		</div>	
+		<div class="col-md-1"></div>
+	</div>
+	<!-- SOCKET DO RODAPÉ -->
+	<div id="divSocket" class="row">
+		<div class="col-md-12 text-center">
+		<img src="img/maxima.png" width="25" height="21">&nbsp;&nbsp;&nbsp;Máxima Sistemas, Versão 4.8.2.3 , Copyright © 2008-2017 Todos os direitos reservados.
+		</div>
+	</div>
+	
+	</div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
