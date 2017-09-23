1cc36e505c489f9b30aa5f4c756080dd6b1195f1
Raul Kaio <rkaio18@gmail.com>
2017-09-22 21:51:59 -0300

    .
    Criada página de produto individual sem logar.

---
+++ produtoIndividualSemLogar.php
@@ -0,0 +1,124 @@
+<?php include 'header.php' ?>
+			
+	<div id="divBreadcrumbs" class="row">
+		<div class="col-md-1"></div>
+		<div class="col-md-4"><a href="">Início</a> > <a href="">Pintura</a> > <a href="">Econômica</a> > <strong>3 EM 1 FOSCO 18L BRANCO</strong></div>
+		<div class="col-md-4 text-center">
+				<div class="btn-group dropdown" role="group">
+					  <button class="btn btn-default dropdown-toggle btnFiltro" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+						FILIAL 1 - <strong>DISTRIBUIDORA LOPES MT</strong>
+						<span class="caret"></span>
+					  </button>
+					  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+						<li><a href="#">FILIAL 1 - <strong>DISTRIBUIDORA LOPES MT</strong></a></li>
+						<li><a href="#">FILIAL 2 - <strong>DISTRIBUIDORA LOPES MS</strong></a></li>
+					  </ul>
+					</div></div>
+		<div class="col-md-2"></div>
+		<div class="col-md-1"></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-md-1"></div>
+		<div class="col-md-2 text-center">
+			<img class="text-center" src="img/fotoProduto.jpg" height="200" width="200">
+		</div>
+		<div class="col-md-5">
+			<h1>3 EM 1 FOSCO 18L BRANCO</h1>
+			<label>Embalagem:</label> <strong>01 UN</strong><br>
+			<label>Unidade:</label> <strong>LT</strong><br>
+			<label>Código:</label> <strong>20830</strong><br>
+			<label>Código de barras:</label> <strong>7891019100033</strong><br><br>
+			<center>
+				<button type="button" class="btn btn-primary btn-lg btn-block text-center btnProdutoFull">
+				<img class="text-center" src="img/icon/digital.png" height="20" width="20">&nbsp;<strong>REGISTRAR-SE</strong><BR>PARA COMPRAR
+				</button>
+			</center>
+		</div>
+		<div class="col-md-2">
+			<div class="row">
+					<div class="col-md-12 text-center">
+						<h2>Aproveite também</h2>
+					</div>
+					<div id="divProdutoMini" class="col-md-12">
+						<center>
+						<a href="#">
+						<img class="text-center" src="img/fotoProduto.jpg" height="100" width="100">
+						<h4>3 EM 1 FOSCO 18L BRANCO</h4></a>
+						<small>Qt. Disp: 9</small>
+						<button type="button" class="btn btn-primary btn-lg btn-block btnProdutoMini">
+						<img class="text-center" src="img/icon/digital.png" height="20" width="20">&nbsp;<strong>REGISTRAR-SE</strong><BR>PARA COMPRAR</button>
+						</center>
+					</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+	<div class="col-md-1"></div>
+	<div class="col-md-11">
+		<h2>Outras pessoas também compraram</h2>
+		<div class="row">
+					<div id="divProdutoGrid" class="col-md-2">
+						<a class="labelCaixa">
+							<img class="text-center iconeCaixa" src="img/icon/caixa.png" height="15" width="15">
+							<span class="text">EMB<BR>04 UN</span>
+						</a>
+						<center>
+						<a href="#">
+						<img class="text-center" src="img/fotoProduto.jpg" height="150" width="150">
+						<h3>3 EM 1 FOSCO 18L BRANCO</h3></a>
+						<small>Qt. Disp: 9</small>
+						<br>&nbsp;
+						<button type="button" class="btn btn-primary btn-lg btn-block btnProdutoFull">
+						<img class="text-center" src="img/icon/digital.png" height="20" width="20">&nbsp;<strong>REGISTRAR-SE</strong><BR>PARA COMPRAR</button>
+						</center>
+					</div>
+					
+					<div id="divProdutoGrid" class="col-md-2">
+						<center>
+						<a href="#">
+						<img class="text-center" src="img/fotoProduto.jpg" height="150" width="150">
+						<h3>3 EM 1 FOSCO 18L BRANCO</h3></a>
+						<small>Qt. Disp: 9</small>
+						<br>&nbsp;
+						<button type="button" class="btn btn-primary btn-lg btn-block btnProdutoFull">
+						<img class="text-center" src="img/icon/digital.png" height="20" width="20">&nbsp;<strong>REGISTRAR-SE</strong><BR>PARA COMPRAR</button>
+						</center>
+					</div>
+					
+					<div id="divProdutoGrid" class="col-md-2">
+						<a class="labelCaixa">
+							<img class="text-center iconeCaixa" src="img/icon/caixa.png" height="15" width="15">
+							<span class="text">EMB<BR>04 UN</span>
+						</a>
+						<center>
+						<a href="#">
+						<img class="text-center" src="img/fotoProduto.jpg" height="150" width="150">
+						<h3>3 EM 1 FOSCO 18L BRANCO</h3></a>
+						<small>Qt. Disp: 9</small>
+						<br>&nbsp;
+						<button type="button" class="btn btn-primary btn-lg btn-block btnProdutoFull">
+						<img class="text-center" src="img/icon/digital.png" height="20" width="20">&nbsp;<strong>REGISTRAR-SE</strong><BR>PARA COMPRAR</button>
+						</center>
+					</div>
+					
+					<div id="divProdutoGrid" class="col-md-2">
+						<a class="labelCaixa">
+							<img class="text-center iconeCaixa" src="img/icon/caixa.png" height="15" width="15">
+							<span class="text">EMB<BR>04 UN</span>
+						</a>
+						<center>
+						<a href="#">
+						<img class="text-center" src="img/fotoProduto.jpg" height="150" width="150">
+						<h3>3 EM 1 FOSCO 18L BRANCO</h3></a>
+						<small>Qt. Disp: 9</small>
+						<br>&nbsp;
+						<button type="button" class="btn btn-primary btn-lg btn-block btnProdutoFull">
+						<img class="text-center" src="img/icon/digital.png" height="20" width="20">&nbsp;<strong>REGISTRAR-SE</strong><BR>PARA COMPRAR</button>
+						</center>
+					</div>
+				</div>
+	</div>
+	</div>
+	<br>
+<?php include 'footer.php'?>
